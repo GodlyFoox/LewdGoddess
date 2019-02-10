7edbfb402777466ad0d3251d4074f536af06b182
GodlyFoox <40329959+GodlyFoox@users.noreply.github.com>
2019-02-10 15:54:49 -0800

    Delete Love.js

--- Commands/Love.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    return message.channel.send("I Love you Darkness, don't give up and I won't give up, we are in this together. :heart:! **Note that this message never changes it is for someone I care about so it will stay**");
-}
-
-module.exports.help = {
-  name: "love"
-}
